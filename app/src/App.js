ec84be7ba8cccd738a9df4afba683cbff80ca57c
e2-levinskas <edgaras2.levinskas@live.uwe.ac.uk>
2023-03-04 14:07:49 +0000

    Tests

--- app/src/App.js
+++ app/src/App.js
@@ -2,29 +2,9 @@
 import logo from './graphics/logo.png';
 import './App.css';
 import './'
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dont Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
+
 function TestHtml() {
+  DBConnect();
   return (
     <body>
       <div class="test">
